9168a114724ca783a59f12a32b541a3a86293d28
mamin11 <abdimamin@gmail.com>
2022-05-18 22:07:09 +0100

    chore: remove ant design and buefy

--- src/main.js
+++ src/main.js
@@ -3,23 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
 import './assets/index.css'
 import VueSession from 'vue-session'
-
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-// import {TimePicker} from 'ant-design-vue'
-// Vue.component(TimePicker.name, TimePicker)
 
 Vue.config.productionTip = false
 
 var options = {
   persist: true
 }
-Vue.use(Antd)
-Vue.use(Buefy)
 Vue.use(VueSession, options)
 
 
